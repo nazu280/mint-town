c6f08215cca0988938ba65657fe1ca54d3c0e0af
nazu280 <66703146+nazu280@users.noreply.github.com>
2022-05-16 21:12:44 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html>
+
+  <head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <style>
+      html, body {
+        background-color: #242424;
+        color: #ffffff;
+      }
+      
+      .searchBox {
+        margin: 20px 0;
+      }
+
+      .listBox {
+        max-width: 500px;
+      }
+
+      .listInner {
+        background-color: #99FFCC;
+        margin: 5px 5px;
+        padding: 10px;
+      }
+
+      .이름 {
+        font-weight: bold;
+      }
+
+      .search {
+        border-radius: 10px;
+        height: 40px;
+        width: 20%;
+        border: 3px solid #99FFCC;
+        cursor: pointer;
+        background-color: #333333;
+        color: #ffffff;
+        text-align: center;
+        white-space: nowrap;
+      }
+
+      .search:focus {
+        cursor: text;
+        animation-name: sea;
+        animation-duration: 0.2s;
+        animation-timing-function: linear;
+        animation-delay: 0s;
+        animation-iteration-count: 1;
+        animation-fill-mode: both;
+        animation-play-state: running;
+      }
+
+      .search:hover {
+        border: 3px solid #66ad8a;
+      }
+    </style>
+  </head>
+
+  <body>
+    <div style="text-align:center">
+    <div class="searchBox">
+      <label><input type="text" id="search" onkeyup="filter()" placeholder="검색" onclick="document.getElementById('search').focus()"></label>
+
+    </div>
+
+    <div class="listBox">
+      <div class="listInner">
+        <span class="이름">네즈 </span>
+        <span class="정보">0x69757f9F880725F2b1E86a28519aDd9A83e67C27</span>
+      </div>
+    </div>
+    </div>
+
+    <script>
+      function filter() {
+        let search = document.getElementById("search").value.toLowerCase();
+        let listInner = document.getElementsByClassName("listInner");
+
+        for (let i = 0; i < listInner.length; i++) {
+          city = listInner[i].getElementsByClassName("이름");
+          country = listInner[i].getElementsByClassName("정보");
+          if (city[0].innerHTML.toLowerCase().indexOf(search) != -1 ||
+            country[0].innerHTML.toLowerCase().indexOf(search) != -1
+          ) {
+            listInner[i].style.display = "flex"
+          } else {
+            listInner[i].style.display = "none"
+          }
+        }
+      }
+
+    </script>
+  </body>
+
+</html>
